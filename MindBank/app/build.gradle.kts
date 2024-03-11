c9aa95053f714233437753e61bd1c226ade8c79a
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-03-11 22:01:03 +0900

    Hilt, Room 초기 설정 진행
    - Gradle Settings

--- MindBank/app/build.gradle.kts
+++ MindBank/app/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
@@ -50,7 +52,10 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.room:room-runtime:2.5.2")
+    kapt("androidx.room:room-compiler:2.5.2")
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,4 +71,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+kapt {
+    correctErrorTypes = true
 }
---
+++ MindBank/app/src/main/java/com/example/mindbank/CoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mindbank
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoreApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
