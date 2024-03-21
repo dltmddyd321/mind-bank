7307f9a77fe87cf013db2308bf9ab4e7230cf715
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-03-21 22:01:34 +0900

    뒤로 가기 선택 다이얼로그 구현
    - BackHandler를 활용하여 사용자에게 의도를 묻는 AlertDialog 팝업

--- MindBank/app/src/main/java/com/example/mindbank/activity/AddActivity.kt
+++ MindBank/app/src/main/java/com/example/mindbank/activity/AddActivity.kt
@@ -1,14 +1,23 @@
 package com.example.mindbank.activity
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mindbank.activity.ui.theme.MindBankTheme
 
@@ -22,11 +31,39 @@ class AddActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BackHandlerWithQuestionDialog()
                 }
             }
         }
     }
+}
+
+@Composable
+fun BackHandlerWithQuestionDialog() {
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+    val activity = LocalContext.current as? Activity
+
+    if (showDialog) {
+        AlertDialog(onDismissRequest = {
+            showDialog = false
+        }, confirmButton = {
+            TextButton(
+                onClick = { activity?.finish() }
+            ) { Text("확인") }
+        }, title = {
+            Text(text = "메모 추가 취소")
+        }, text = {
+            Text(text = "메모 추가를 취소하시겠습니까?")
+        }, dismissButton = {
+            TextButton(
+                onClick = { showDialog = false }
+            ) { Text("취소") }
+        })
+    }
+
+    BackHandler { showDialog = true }
 }
 
 @Composable
