3071a22563b3709e6ce17a6984c34a9eecd56875
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-04-18 20:18:13 +0900

    비밀번호 잠금 화면 구성

--- MindBank/app/src/main/java/com/example/mindbank/activity/PasswordActivity.kt
+++ MindBank/app/src/main/java/com/example/mindbank/activity/PasswordActivity.kt
@@ -3,24 +3,37 @@ package com.example.mindbank.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,17 +49,56 @@ class PasswordActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PasswordScreen()
                 }
             }
         }
     }
 }
 
+@Composable
+fun PasswordScreen() {
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "비밀번호 입력", color = Color.White, fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        PasswordDots(length = password.length)
+        Spacer(modifier = Modifier.height(32.dp))
+        NumberPad { number ->
+            password += number
+        }
+    }
+}
+
+@Composable
+fun PasswordDots(length: Int) {
+    Row {
+        repeat(4) {
+            Icon(
+                imageVector = if (it <= length - 1) Icons.Filled.Info else Icons.Filled.CheckCircle,
+                contentDescription = null,
+                tint = Color.Blue,
+                modifier = Modifier.padding(4.dp)
+            )
+        }
+    }
+}
 
 @Composable
 fun NumberPad(onNumberClick: (String) -> Unit) {
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         // 숫자 버튼들
         val numbers = arrayOf(
             arrayOf("1", "2", "3"),
@@ -56,7 +108,10 @@ fun NumberPad(onNumberClick: (String) -> Unit) {
 
         // 숫자 버튼을 표시하는 그리드
         numbers.forEach { row ->
-            Row {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
                 row.forEach { number ->
                     NumberButton(number) {
                         onNumberClick(number)
@@ -65,7 +120,10 @@ fun NumberPad(onNumberClick: (String) -> Unit) {
             }
         }
 
-        Row {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
             // 빈 공간
             Spacer(modifier = Modifier.weight(1f))
             // 숫자 0 버튼
@@ -83,7 +141,7 @@ fun NumberButton(num: String, onClick: () -> Unit) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
-            .size(64.dp)
+            .size(84.dp)
             .padding(8.dp)
             .clip(CircleShape)
             .clickable { onClick() }
@@ -92,7 +150,8 @@ fun NumberButton(num: String, onClick: () -> Unit) {
         Text(
             text = num,
             color = Color.White,
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center
         )
     }
 }
