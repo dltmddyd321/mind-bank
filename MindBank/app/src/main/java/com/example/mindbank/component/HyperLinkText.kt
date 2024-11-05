425f4b1c497e2eced06b3e1a29dc88cc3192a6f8
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-11-05 21:18:44 +0900

    링크 텍스트 지워지는 문제 수정

--- MindBank/app/src/main/java/com/example/mindbank/component/HyperLinkText.kt
+++ MindBank/app/src/main/java/com/example/mindbank/component/HyperLinkText.kt
@@ -2,6 +2,7 @@ package com.example.mindbank.component
 
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
@@ -21,35 +22,37 @@ fun HyperlinkText(
     val linkRegex = "(https?://[\\w-]+(\\.[\\w-]+)+(:\\d+)?(/\\S*)?)".toRegex()
 
     // AnnotatedString을 통해 링크 부분에 annotation 추가
-    val annotatedString = buildAnnotatedString {
-        var lastIndex = 0
-        linkRegex.findAll(text).forEach { matchResult ->
-            val startIndex = matchResult.range.first
-            val endIndex = matchResult.range.last + 1
-            val linkText = matchResult.value
+    val annotatedString = remember(text) {
+        buildAnnotatedString {
+            var lastIndex = 0
+            linkRegex.findAll(text).forEach { matchResult ->
+                val startIndex = matchResult.range.first
+                val endIndex = matchResult.range.last + 1
+                val linkText = matchResult.value
 
-            // 링크가 아닌 일반 텍스트 추가
-            append(text.substring(lastIndex, startIndex))
+                // 링크가 아닌 일반 텍스트 추가
+                append(text.substring(lastIndex, startIndex))
 
-            // 링크 텍스트 추가
-            pushStringAnnotation(
-                tag = "URL", // 사용할 태그 이름
-                annotation = linkText // 링크 텍스트를 annotation으로 추가
-            )
-            withStyle(
-                style = SpanStyle(
-                    color = Color.Blue,
-                    textDecoration = TextDecoration.Underline
+                // 링크 텍스트 추가
+                pushStringAnnotation(
+                    tag = "URL", // 사용할 태그 이름
+                    annotation = linkText // 링크 텍스트를 annotation으로 추가
                 )
-            ) {
-                append(linkText)
-            }
-            pop()
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Blue,
+                        textDecoration = TextDecoration.Underline
+                    )
+                ) {
+                    append(linkText)
+                }
+                pop()
 
-            lastIndex = endIndex
+                lastIndex = endIndex
+            }
+            // 마지막 남은 텍스트 추가
+            append(text.substring(lastIndex, text.length))
         }
-        // 마지막 남은 텍스트 추가
-        append(text.substring(lastIndex, text.length))
     }
 
     // ClickableText를 이용해 annotation을 감지하고 링크 클릭 처리
