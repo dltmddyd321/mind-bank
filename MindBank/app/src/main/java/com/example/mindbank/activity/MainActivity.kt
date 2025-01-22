49d9ab7549757071e87412ba127e966d190690d0
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-01-22 22:11:23 +0900

    2230 풀이

--- MindBank/app/src/main/java/com/example/mindbank/activity/MainActivity.kt
+++ MindBank/app/src/main/java/com/example/mindbank/activity/MainActivity.kt
@@ -15,12 +15,20 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.navigation.compose.rememberNavController
-import com.example.mindbank.viewmodel.DataViewModel
 import com.example.mindbank.navigation.BottomNavBar
 import com.example.mindbank.navigation.NavigationGraph
 import com.example.mindbank.viewmodel.AdviceViewModel
+import com.example.mindbank.viewmodel.DataViewModel
 import com.example.mindbank.viewmodel.TodoViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.Arrays
+import java.util.StringTokenizer
+import kotlin.math.min
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -71,3 +79,41 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Throws(Exception::class)
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val st = StringTokenizer(br.readLine())
+
+    val N = st.nextToken().toInt()
+    val M = st.nextToken().toInt()
+    val arr = IntArray(N)
+    for (i in 0 until N) {
+        arr[i] = br.readLine().toInt()
+    }
+    Arrays.sort(arr)
+
+    var i = 0
+    var j = 0
+    var ans = Int.MAX_VALUE
+    // 투 포인터 알고리즘
+    while (i < N) {
+        if (arr[i] - arr[j] < M) {
+            i++
+            continue
+        }
+
+        if (arr[i] - arr[j] == M) {
+            ans = M
+            break
+        }
+
+        ans = min(ans.toDouble(), (arr[i] - arr[j]).toDouble()).toInt()
+        j++
+    }
+
+    bw.write(ans.toString() + "\n")
+    bw.flush()
+    bw.close()
+    br.close()
+}
