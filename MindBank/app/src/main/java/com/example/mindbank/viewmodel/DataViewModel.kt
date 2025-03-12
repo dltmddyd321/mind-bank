67adac8a89884618a64d6c5f13d0758045f4edb0
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-03-12 21:58:22 +0900

    메모 StateFlow 처리

--- MindBank/app/src/main/java/com/example/mindbank/viewmodel/DataViewModel.kt
+++ MindBank/app/src/main/java/com/example/mindbank/viewmodel/DataViewModel.kt
@@ -3,8 +3,14 @@ package com.example.mindbank.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mindbank.data.SaveData
+import com.example.mindbank.data.Task
 import com.example.mindbank.db.DataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -12,6 +18,20 @@ import javax.inject.Inject
 class DataViewModel @Inject constructor(
     private val dataRepository: DataRepository
 ) : ViewModel() {
+
+    private val _memos = MutableStateFlow<List<SaveData>>(emptyList())
+    val memos: StateFlow<List<SaveData>> = _memos.asStateFlow()
+
+    init {
+        loadMemoList()
+    }
+
+    fun loadMemoList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val data = getAllData()
+            _memos.value = data
+        }
+    }
 
     private val memoComparator = compareByDescending<SaveData> { it.dtCreated }
 
@@ -28,11 +48,17 @@ class DataViewModel @Inject constructor(
     }
 
     fun insertData(data: SaveData) {
-        dataRepository.insertOrUpdate(data)
+        viewModelScope.launch(Dispatchers.IO) {
+            dataRepository.insertOrUpdate(data)
+            loadMemoList()
+        }
     }
 
     fun deleteData(data: SaveData) {
-        dataRepository.delete(data)
+        viewModelScope.launch(Dispatchers.IO) {
+            dataRepository.delete(data)
+            loadMemoList()
+        }
     }
 
     fun clear() {
