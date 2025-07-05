56e0fdbefc73f08da6186787f19366a412002e8e
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2025-07-05 12:37:58 +0900

    Update build.gradle.kts

--- MindBank/app/build.gradle.kts
+++ MindBank/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.windrr.mindbank"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
